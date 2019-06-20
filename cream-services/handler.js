d0cb662e1bc0379e3c5f883d996ca95e753bc868
PAUL SMOUT <paul.smout@gtb.com>
2019-06-20 19:57:33 +0800

    Added random numbers to fields

--- cream-services/handler.js
+++ cream-services/handler.js
@@ -3,6 +3,9 @@ export const latest = async (event, context) => {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true
     };
+
+  let rand = Math.random() * 0.1;
+  
   return {
     statusCode: 200,
     headers : headers,
@@ -10,10 +13,10 @@ export const latest = async (event, context) => {
       "device-id":"46820256-0ed7-4895-b2d4-6691a081b2a5",
       "reading-time" :"tbd",
        "reading" : {
-            "voltage-V":123,
-            "current-A": 5.6,
-             "active-power-W": 688.8,
-             "accumulated-power-kWh":9999,
+            "voltage-V": Math.floor(123 * rand),
+            "current-A": 5.6 * rand,
+             "active-power-W": Math.floor(688.8 * rand),
+             "accumulated-power-kWh": Math.floor(9999 * rand),
              "frequency-Hz": 50,
              "power-factor": 0.9
        }
